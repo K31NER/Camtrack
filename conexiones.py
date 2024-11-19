952077d4db42484d1a2cf1b9b44569770721006c
keiner zr <146565072+K31NER@users.noreply.github.com>
2024-11-19 07:09:16 -0500

    cambio de conexiones.py-1

--- conexiones.py
+++ conexiones.py
@@ -1,2 +1,2 @@
 #url de el esp32cam este se debe actualizar cada que se ejecute el codigo ya que la direccion ip cambia
-url = "http://192.168.101.9:81/stream"
+url = "http://192.168.23.77:81/stream"
