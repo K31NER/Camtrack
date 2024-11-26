2e1b1a19e6f21265028b13755add3186070aea0e
keiner zr <146565072+K31NER@users.noreply.github.com>
2024-11-26 18:55:47 -0500

    Camara por defecto
    Se comento la url para definir la camara por defecto, esto se debe cambiar si se desea trabajar con el esp32 cam

--- conexiones.py
+++ conexiones.py
@@ -1,2 +1,6 @@
 #url de el esp32cam este se debe actualizar cada que se ejecute el codigo ya que la direccion ip cambia
-url = "http://192.168.23.77:81/stream"
+
+#Se comenta el url ya que al no tener esp32 cam usaremos la camara por defecto del dispositivo
+#url = "http://192.168.23.77:81/stream"
+url = 0
+
